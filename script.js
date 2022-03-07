639ac64ff55253083ef469132542fa648a8e1a5e
“DrewClabes” <dclabes7@gmail.com>
2022-03-07 16:44:12 -0500

    added JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,65 @@
+// 1
+console.log(55);
+
+//  2
+/*
+Strings
+Boolean
+Numbers
+Null
+Undefined
+*/
+
+// 3
+// Addition is +
+// Subtraction is -
+// Multiplication is *
+// Division is /
+// Modulus is %
+// Exponentiation  is **
+
+// 4
+console.log(5 % 4); // 1
+console.log(5 % 2); //1
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Drew".length + "Clabes".length); // 10
+
+// 8
+console.log("Avengers: Endgame"(16));
+
+// Breakdown
+// console.log("Avengers: Endgame"(16));
+// console.log("Avengers: Endgame".length);
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log((5 ** 4)-9);
+// 
+// console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length + "AM".length % "Iron".length - "Man".length);
+
+// console.log(("I".length + "AM".length + "Iron".length + "Man".length) % 10);
+
+// console.log(("I".length - "AM".length) + ("Iron".length % "Man".length));
+
+// 4
+console.log("CAP"(1));
+console.log("VISION"(0));
+console.log("OKOYE"(4));
+console.log("NICK FURY"(0));
+console.log("GROOT"(0));
+console.log("DOCTOR STRANGE"(13));
+console.log("THOR"(3));
+console.log("STAR-LORD"(0));
